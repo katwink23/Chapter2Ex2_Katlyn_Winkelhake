f660a4cc884939a6421810f00d79fbbddea4c3f6
Katlyn W. <krw8ateez@gmail.com>
2024-08-30 10:33:28 -0400

    changed /n to endl

--- Chapter2Ex2_Katlyn_Winkelhake.cpp
+++ Chapter2Ex2_Katlyn_Winkelhake.cpp
@@ -16,12 +16,11 @@ int main()
 	int number1;
 	int number2;
 	int answer;
-	cout << "Please enter a number.\n";
+	cout << "Please enter a number." << endl;
 	cin >> number1;
-	cout << "Please enter another number.\n";
+	cout << "Please enter another number." << endl;
 	cin >> number2;
 	answer = number1 + number2;
 	cout << "The sum of " << number1 << " and " << number2 << " is " << answer;
    return 0;
 }
-
