62cd12ac264fd8a932352c774fad1f322b939877
Katlyn W. <krw8ateez@gmail.com>
2024-08-30 10:28:05 -0400

    Add project files.

---
+++ Chapter2Ex2_Katlyn_Winkelhake.cpp
@@ -0,0 +1,27 @@
+
+/* 
+Title: Chapter2Ex2
+File Name: Chapter2Ex2_Katlyn_Winkelhake.cpp
+Programmer: Katlyn Winkelhake
+Date: Aug 30 2024
+Requirements: Write a program that will predict how much the division will generate if the company has $8.6 million in sales this year.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number1;
+	int number2;
+	int answer;
+	cout << "Please enter a number.\n";
+	cin >> number1;
+	cout << "Please enter another number.\n";
+	cin >> number2;
+	answer = number1 + number2;
+	cout << "The sum of " << number1 << " and " << number2 << " is " << answer;
+   return 0;
+}
+
